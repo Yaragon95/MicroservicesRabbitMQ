8a42c46262810a7658fe4bd8aa2044d567ed3cdb
Ysaragon <Ysaragon95@gmail.com>
2023-12-28 22:37:39 -0500

    Add project files.

---
+++ MicroRabbit.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,30 @@
+﻿using MicroRabbit.Domain.Core.Commands;
+using MicroRabbit.Domain.Core.Events;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        /// <summary>
+        /// Método para enviar objetos de tipo Command (Mediator)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        Task SendCommand<T>(T command) where T : Command;
+
+        /// <summary>
+        /// Método que permite publicar mensajes en RabbitMQ
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="event"></param>
+        void Publish<T>(T @event) where T : Event;
+
+        /// <summary>
+        /// Método para consumir los mensajes de RabbitMQ
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TH"></typeparam>
+        void Subscribe<T, TH>() where T : Event where TH : IEventHandler<T>;
+    }
+}
---
+++ MicroRabbit.Domain.Core/Commands/Command.cs
@@ -0,0 +1,15 @@
+﻿namespace MicroRabbit.Domain.Core.Commands
+{
+    /// <summary>
+    /// Referencia del mensaje que sera transportado
+    /// </summary>
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; set; }
+
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
